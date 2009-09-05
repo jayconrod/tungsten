ddaef542fde7df1d20c6300d2c503839213fdd4b
conrod <conrod@1b10a55c-1d33-0410-9319-dc433ba633da>
2009-09-05 02:47:04 +0000

    modified tokens to include Location
    git-svn-id: http://svn.jayconrod.com/code/tungsten@640 1b10a55c-1d33-0410-9319-dc433ba633da

--- src/tungsten/AstLexer.scala
+++ src/tungsten/AstLexer.scala
@@ -10,28 +10,40 @@ object AstLexer extends Parsers {
 
   def whitespace = rep(whitespaceChar)
 
-  def errorToken(msg: String) = ErrorToken(msg)
+  def errorToken(msg: String, loc: Location) = ErrorToken(msg, loc)
 
   def token: Parser[Token] = failure("illegal character")
 
-  class Scanner(in: Reader[Elem]) extends Reader[Token] {
-    def this(in: String) = this(new CharArrayReader(in.toCharArray))
+  def test(in: String) = {
+    val reader = new CharArrayReader(in.toCharArray)
+    val result = phrase(token)(reader)
+    result.get
+  }
+
+  class Scanner(filename: String, in: Reader[Elem]) extends Reader[Token] {
+    def this(in: String) = this("<UNKNOWN>", new CharArrayReader(in.toCharArray))
+
+    implicit private def locationFromPosition(pos: Position): Location = {
+      new Location(filename, pos.line, pos.column, pos.line, pos.column + 1)
+    }
 
     private val (tok, rest1, rest2) = whitespace(in) match {
       case Success(_, in1) => {
         token(in1) match {
           case Success(tok, in2) => (tok, in1, in2)
-          case ns: NoSuccess => (errorToken(ns.msg), ns.next, skip(ns.next))
+          case ns: NoSuccess => {
+            (errorToken(ns.msg, ns.next.pos), ns.next, skip(ns.next))
+          }
         }
       }
-      case ns: NoSuccess => (errorToken(ns.msg), ns.next, skip(ns.next))
+      case ns: NoSuccess => (errorToken(ns.msg, ns.next.pos), ns.next, skip(ns.next))
     }
     private def skip(in: Reader[Elem]) = if (in.atEnd) in else in.rest
 
     override def source = in.source
     override def offset = in.offset
     def first = tok
-    def rest = new Scanner(rest2)
+    def rest = new Scanner(filename, rest2)
     def pos = rest1.pos
     def atEnd = {
       if (in.atEnd)
--- src/tungsten/Tokens.scala
+++ src/tungsten/Tokens.scala
@@ -1,4 +1,6 @@
 package tungsten
 
-sealed abstract class Token
-final case class ErrorToken(msg: String) extends Token
+sealed abstract class Token(val location: Location)
+
+final case class ErrorToken(msg: String, loc: Location) extends Token(loc)
+final case class ReservedToken(val text: String, loc: Location) extends Token(loc)

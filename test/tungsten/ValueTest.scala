36f044ff61f00151c557d1d7cac15d49ae64328d
conrod <conrod@1b10a55c-1d33-0410-9319-dc433ba633da>
2009-10-11 19:34:43 +0000

    added ValueTest
    git-svn-id: http://svn.jayconrod.com/code/tungsten@674 1b10a55c-1d33-0410-9319-dc433ba633da

---
+++ test/tungsten/ValueTest.scala
@@ -0,0 +1,29 @@
+package tungsten
+
+import org.junit.Test
+import org.junit.Assert._
+
+class ValueTest {
+  val loc = Location("foo.w", 1, 2, 3, 4)
+  val module = new Module
+
+  @Test
+  def unit = {
+    assertEquals(UnitType(), UnitValue(loc).ty(module))
+  }
+
+  @Test
+  def int = {
+    assertEquals(IntType(32), Int32Value(12, loc).ty(module))
+  }
+
+  @Test
+  def global = {
+    val foo = new Symbol("foo")
+    val global = Global(foo, UnitType(), Some(UnitValue()))
+    val module = new Module
+    module.add(global)
+
+    assertEquals(UnitType(), DefinedValue(foo).ty(module))
+  }
+}

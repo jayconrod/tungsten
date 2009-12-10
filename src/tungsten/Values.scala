fd8741505a0dea232086d2ab3f16eee91e82381e
conrod <conrod@1b10a55c-1d33-0410-9319-dc433ba633da>
2009-12-10 02:04:12 +0000

    Added toString for Value classes
    git-svn-id: http://svn.jayconrod.com/code/tungsten@727 1b10a55c-1d33-0410-9319-dc433ba633da

--- src/tungsten/Values.scala
+++ src/tungsten/Values.scala
@@ -14,54 +14,72 @@ sealed abstract class Value(location: Location) extends TungstenObject(location)
 
 final case class UnitValue(override location: Location = Nowhere) extends Value(location) {
   def ty(module: Module): UnitType = UnitType(location)
+
+  override def toString = "()"
 }
 
 final case class BooleanValue(value: Boolean, override location: Location = Nowhere)
   extends Value(location)
 {
   def ty(module: Module): BooleanType = BooleanType(location)
+
+  override def toString = if (value) "#true" else "#false"
 }
 
 final case class Int8Value(value: Byte, override location: Location = Nowhere) 
   extends Value(location)
 {
   def ty(module: Module): IntType = IntType(8, location)
+
+  override def toString = value + "b"
 }
 
 final case class Int16Value(value: Short, override location: Location = Nowhere)
   extends Value(location)
 {
   def ty(module: Module): IntType = IntType(16, location)
+
+  override def toString = value + "s"
 }
 
 final case class Int32Value(value: Int, override location: Location = Nowhere)
   extends Value(location)
 {
   def ty(module: Module): IntType = IntType(32, location)
+
+  override def toString = value.toString
 }
 
 final case class Int64Value(value: Long, override location: Location = Nowhere)
   extends Value(location)
 {
   def ty(module: Module): IntType = IntType(64, location)
+
+  override def toString = value + "L"
 }
 
 final case class Float32Value(value: Float, override location: Location = Nowhere)
   extends Value(location)
 {
   def ty(module: Module): FloatType = FloatType(32, location)
+
+  override def toString = value + "f"
 }
 
 final case class Float64Value(value: Double, override location: Location = Nowhere)
   extends Value(location)
 {
   def ty(module: Module): FloatType = FloatType(64, location)
+
+  override def toString = value.toString
 }
 
 final case class NullValue(override location: Location = Nowhere)
   extends Value(location)
 {
   def ty(module: Module): NullType = NullType(location)
+
+  override def toString = "#null"
 }
 
 final case class ArrayValue(elementType: Type,
@@ -82,6 +100,8 @@ final case class ArrayValue(elementType: Type,
       elem.validateType(module, elementType) ++ errors
     }
   }
+
+  override def toString = "[%s: %s]".format(elementType, elements.mkString(", "))
 }      
 
 final case class DefinedValue(value: Symbol, override location: Location = Nowhere)
@@ -95,4 +115,6 @@ final case class DefinedValue(value: Symbol, override location: Location = Nowhe
       case _ => throw new RuntimeException("symbol " + value + " cannot be used as a value")
     }
   }
+
+  override def toString = value.toString
 }

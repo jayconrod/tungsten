ba396c4683d227fff2a87016bcfaf9ad7b601279
Jay Conrod <jayconrod@gmail.com>
2009-12-18 09:07:31 -0700

    Changed Definition.validateComponentsOfClass to use Module.validateName

--- src/tungsten/Definition.scala
+++ src/tungsten/Definition.scala
@@ -24,19 +24,8 @@ abstract class Definition(val name: Symbol, location: Location = Nowhere)
         case n :: ns => {
           val newErrors = if (seen.contains(n))
             DuplicateComponentException(name, n, className, location) :: errors
-          else {
-            module.get[T](n) match {
-              case Some(defn) if m.erasure.isInstance(defn) => errors
-              case Some(defn) => { 
-                InappropriateSymbolException(n, 
-                                             location,
-                                             defn.location,
-                                             className) :: errors
-              }
-              case None => UndefinedSymbolException(n, location) :: errors
-            }
-          }
-
+          else
+            module.validateName[T](n, location) ++ errors
           check(ns, seen + n, newErrors)
         }
       }

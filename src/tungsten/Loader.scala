0046106892e5241a336a044e0f30eb69367abe84
Jay Conrod <jay@marble.(none)>
2010-01-31 11:27:10 -0800

    Moved module loading code from Interpreter to Loader and added validation

---
+++ src/tungsten/Loader.scala
@@ -0,0 +1,82 @@
+package tungsten
+
+import scala.util.matching.Regex
+import java.io._
+import Utilities._
+
+final object Loader {
+  def loadAndLinkProgram(file: File) = {
+    val modules = loadModuleAndDependencies(file, ModuleType.PROGRAM, Version.MIN, Version.MAX)
+    
+    val program = Linker.linkModules(modules,
+                                     "default",
+                                     ModuleType.PROGRAM,
+                                     Version.MIN,
+                                     Nil,
+                                     Nil)
+    program
+  }
+
+  def loadModuleAndDependencies(file: File,
+                                ty: ModuleType,
+                                minVersion: Version,
+                                maxVersion: Version): List[Module] =
+  {
+    val module = ModuleIO.readBinary(file)
+    if (module.ty != ty)
+      throw new IOException("file %s was supposed to be a %s module".format(file.getName, ty))
+    if (module.version < minVersion) {
+      throw new IOException("library %s has version %s which is less than the minimum, %s".
+                          format(file, module.version, minVersion))
+    }
+    if (module.version > maxVersion) {
+      throw new IOException("library %s has version %s which is greater than the maximum, %s".
+                          format(file, module.version, maxVersion))
+    }
+    val errors = module.validate
+    if (!errors.isEmpty) {
+      throw new IOException("validation errors in module %s:\n%s".
+                          format(file, errors.mkString("\n")))
+    }
+
+    val libraries = module.dependencies.flatMap { dependency =>
+      val libraryFile = findModuleFile(dependency, module.searchPaths)
+      loadModuleAndDependencies(libraryFile, 
+                                ModuleType.LIBRARY, 
+                                dependency.minVersion,
+                                dependency.maxVersion)
+    }
+    module :: libraries
+  }
+
+  def findModuleFile(dependency: ModuleDependency, 
+                     searchPaths: List[File]): File =
+  {
+    val files = for (dir  <- searchPaths.iterator;
+                     file <- dir.listFiles.iterator)
+                  yield file
+
+    def isMatchingLibrary(file: File): Boolean = {
+      file.getName match {
+        case LibraryName(nameStr, versionStr) => {
+          try {
+            val libName = symbolFromString(nameStr)
+            val libVersion = parseVersion(versionStr)
+            libName == dependency.name && 
+              dependency.minVersion <= libVersion && libVersion <= dependency.maxVersion
+          } catch {
+            case _ => false
+          }
+        }
+        case _ => false
+      }
+    }
+
+    files.find(isMatchingLibrary _) match {
+      case Some(file) => file
+      case None => throw new IOException("could not find library: " + dependency)
+    }
+  }
+
+  val LibraryName = new Regex("([A-Za-z0-9_$.]+)-([0-9.]+).wl")
+}
--- src/tungsten/interpreter/Interpreter.scala
+++ src/tungsten/interpreter/Interpreter.scala
@@ -1,9 +1,8 @@
 package tungsten.interpreter
 
 import java.io._
-import scala.util.matching.Regex
 import tungsten._
-import tungsten.Utilities._
+import Utilities._
 
 object Interpreter {
   def usage = System.err.println("usage: Interpreter program\n")
@@ -16,83 +15,14 @@ object Interpreter {
 
     var program: Module = null
     try {
-      val file = new File(args(0))
-      val modules = load(file, ModuleType.PROGRAM, Version.MIN, Version.MAX)
-      program = Linker.linkModules(modules,
-                                   "default",
-                                   ModuleType.PROGRAM,
-                                   Version.MIN,
-                                   Nil,
-                                   Nil)
-      val errors = program.validate
-      if (!errors.isEmpty) {
-        errors.foreach(System.err.println(_))
-        System.exit(FAILURE_CODE)
-      }
+      program = Loader.loadAndLinkProgram(new File(args(0)))
     } catch {
       case exn: IOException => exitWithFailure(exn.getMessage)
     }
     assert(program != null)
-
+     
     val env = new Environment(program)
     val errorCode = env.run
     System.exit(errorCode)
   }
-
-  def load(file: File, 
-           ty: ModuleType,
-           minVersion: Version,
-           maxVersion: Version): List[Module] = 
-  {
-    val module = ModuleIO.readBinary(file)
-    if (module.ty != ty)
-      exitWithFailure("file %s was expected to be a %s module".format(file.getName, ty))
-    if (module.version < minVersion) {
-      exitWithFailure("library %s has version %s which is less than the minimum, %s".
-                        format(file, module.version, minVersion))
-    }
-    if (module.version > maxVersion) {
-      exitWithFailure("library %s has version %s which is greater than the maximum, %s".
-                        format(file, module.version, maxVersion))
-    }
-
-    val libraries = module.dependencies.flatMap { dependency =>
-      val libraryFile = findModule(dependency, module.searchPaths)
-      load(libraryFile, ModuleType.LIBRARY, dependency.minVersion, dependency.maxVersion)
-    }
-    module :: libraries
-  }
-
-  def findModule(dependency: ModuleDependency, searchPaths: List[File]): File =
-  {
-    val files = for (dir  <- searchPaths.iterator;
-                     file <- dir.listFiles.iterator)
-                  yield file
-
-    def isMatchingLibrary(file: File): Boolean = {
-      file.getName match {
-        case LibraryName(nameStr, versionStr) => {
-          try {
-            val libName = symbolFromString(nameStr)
-            val libVersion = parseVersion(versionStr)
-            libName == dependency.name && 
-              dependency.minVersion <= libVersion && libVersion <= dependency.maxVersion
-          } catch {
-            case _ => false
-          }
-        }
-        case _ => false
-      }
-    }
-
-    files.find(isMatchingLibrary _) match {
-      case Some(file) => file
-      case None => {
-        exitWithFailure("could not find library: " + dependency)
-        null
-      }
-    }
-  }
-
-  val LibraryName = new Regex("([A-Za-z0-9_$.]+)-([0-9.]+).wl")
 }

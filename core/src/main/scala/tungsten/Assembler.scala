35fc7739115f0273023ec1c64c2eaf30fba40692
Jay Conrod <jayconrod@gmail.com>
2010-04-14 12:25:52 -0700

    Rewrote Assembler and Disassembler to use Converter

--- core/src/main/scala/tungsten/Assembler.scala
+++ core/src/main/scala/tungsten/Assembler.scala
@@ -3,84 +3,24 @@ package tungsten
 import java.io._
 import Utilities._
 
-object Assembler {
-  var errorOccurred = false
-
-  def main(args: Array[String]) = {
-    if (args.isEmpty) {
-      val module = readModuleFromStdin
-      writeModuleToStdout(module)
-    } else {
-      for (filename <- args;
-           val inputFile = new File(filename);
-           module <- readModuleFromFile(inputFile))
-      {
-        val outputFile = fileWithExtension(inputFile, ".w", ".wo")
-        writeModuleToFile(module, outputFile)
-      }
-    }
-
-    if (errorOccurred)
-      System.exit(FAILURE_CODE)
-  }
-
-  def readModuleFromFile(file: File): Option[Module] = {
-    try {
-      ModuleIO.readText(file) match {
-        case Left(module) => Some(module)
-        case Right(errors) => {
-          errors.foreach(System.err.println _)
-          errorOccurred = true
-          None
-        }
-      }
-    } catch {
-      case exn: IOException => {
-        System.err.println(file + ": IO error: " + exn.getMessage)
-        errorOccurred = true
+object Assembler extends Converter[Module, Module] {
+  def readSource(filename: String, input: InputStream): Option[Module] = {
+    val reader = new InputStreamReader(input)
+    ModuleIO.readText(reader, filename) match {
+      case Left(source) => Some(source)
+      case Right(errors) => {
+        errors.foreach(System.err.println _)
         None
       }
     }
   }
 
-  def readModuleFromStdin: Module = {
-    try {
-      val reader = new InputStreamReader(System.in)
-      ModuleIO.readText(reader) match {
-        case Left(module) => module
-        case Right(errors) => {
-          System.err.println("errors while reading <stdin>:")
-          errors.foreach(System.err.println _)
-          System.exit(1)
-          null
-        }
-      }
-    } catch {
-      case exn: IOException => {
-        exitWithFailure("<stdin>: IO error: " + exn.getMessage)
-        null
-      }
-    }
+  def writeTarget(target: Module, filename: String, output: OutputStream) {
+    ModuleIO.writeBinary(target, output)
   }
 
-  def writeModuleToFile(module: Module, file: File) {
-    try {
-      ModuleIO.writeBinary(module, file)
-    } catch {
-      case exn: IOException => {
-        System.err.println(file + ": IO error: " + exn.getMessage)
-        errorOccurred = true
-      }
-    }
-  }
+  def convert(source: Module): Option[Module] = Some(source)
 
-  def writeModuleToStdout(module: Module) {
-    try {
-      ModuleIO.writeBinary(module, System.out)
-    } catch {
-      case exn: IOException => {
-        exitWithFailure("<stdout>: IO error: " + exn.getMessage)
-      }
-    }
-  }
+  def convertFile(sourceFile: File): File = fileWithExtension(sourceFile, ".w", ".wo")
 }
+
--- core/src/main/scala/tungsten/Disassembler.scala
+++ core/src/main/scala/tungsten/Disassembler.scala
@@ -3,69 +3,17 @@ package tungsten
 import java.io._
 import Utilities._
 
-object Disassembler {
-  var errorOccurred = false
-
-  def main(args: Array[String]) = {
-    if (args.isEmpty) {
-      val module = readModuleFromStdin
-      writeModuleToStdout(module)
-    } else {
-      for (filename <- args;
-           val inputFile = new File(filename);
-           module <- readModuleFromFile(inputFile))
-      {
-        val outputFile = fileWithExtension(inputFile, ".wo", ".w")
-        writeModuleToFile(module, outputFile)
-      }
-    }
-
-    if (errorOccurred)
-      System.exit(FAILURE_CODE)
+object Disassembler extends Converter[Module, Module] {
+  def readSource(filename: String, input: InputStream): Option[Module] = {
+    Some(ModuleIO.readBinary(input))
   }
 
-  def readModuleFromFile(file: File): Option[Module] = {
-    try {
-      Some(ModuleIO.readBinary(file))
-    } catch {
-      case exn: IOException => {
-        System.err.println(file + ": IO error: " + exn.getMessage)
-        errorOccurred = true
-        None
-      }
-    }
+  def writeTarget(target: Module, filename: String, output: OutputStream) {
+    val writer = new OutputStreamWriter(output)
+    ModuleIO.writeText(target, writer)
   }
 
-  def readModuleFromStdin: Module = {
-    try {
-      ModuleIO.readBinary(System.in)
-    } catch {
-      case exn: IOException => {
-        exitWithFailure("<stdin>: IO error: " + exn)
-        null
-      }
-    }
-  }
+  def convert(source: Module): Option[Module] = Some(source)
 
-  def writeModuleToFile(module: Module, file: File) {
-    try {
-      ModuleIO.writeText(module, file)
-    } catch {
-      case exn: IOException => {
-        System.err.println(file + ": IO error: " + exn.getMessage)
-        errorOccurred = true
-      }
-    }
-  }
-
-  def writeModuleToStdout(module: Module) {
-    val writer = new OutputStreamWriter(System.out)
-    try {
-      ModuleIO.writeText(module, writer)
-    } catch {
-      case exn: IOException => {
-        exitWithFailure("<stdout>: IO error: " + exn.getMessage)
-      }
-    }
-  }
+  def convertFile(sourceFile: File): File = fileWithExtension(sourceFile, ".wo", ".w")
 }

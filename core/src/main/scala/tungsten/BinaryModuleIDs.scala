a35fb66b63c26874dec4534f1adec5b93ce60274
Jay Conrod <jayconrod@gmail.com>
2011-06-08 22:58:40 -0700

    Added LGPL notice to all source files

--- core/src/main/scala/tungsten/BinaryModuleIDs.scala
+++ core/src/main/scala/tungsten/BinaryModuleIDs.scala
@@ -1,3 +1,22 @@
+/* Copyright 2009-2011 Jay Conrod
+ *
+ * This file is part of Tungsten.
+ *
+ * Tungsten is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2 of 
+ * the License, or (at your option) any later version.
+ *
+ * Tungsten is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with Tungsten.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package tungsten
 
 object BinaryModuleIDs {
